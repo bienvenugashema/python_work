ccca5f0d12b0dc7d30d8e13f39fe5a1433ce28d8
MWIMULE Bienvenu <bienvenugashema@gmail.com>
2025-03-17 20:08:19 +0400

    version 4

--- python.py
+++ python.py
@@ -4,3 +4,15 @@
 # words = input("Enter your words: ").replace(" ", "")
 # print(len(words))
 # print(words)
+
+
+array = []
+x = 0
+words = input("Enter five words words: ").split()
+if len(words) != 5:
+    print("You must enter five words")
+else:    
+    for word in words:
+        array.append(f"{word} => {len(word)}")
+print(array)
+
